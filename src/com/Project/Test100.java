af24d552e38add84bd277d075fd3211640d9892c
madhu <madhu@Madhu>
2024-07-16 12:59:49 +0530

    second commit

---
+++ src/com/Project/Test100.java
@@ -0,0 +1,10 @@
+package com.Project;
+public class Test100 {
+
+	public static void main(String[] args) {
+		System.out.println("my name is madhu");
+		// TODO Auto-generated method stub
+
+	}
+
+}
