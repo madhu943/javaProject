103cb83dccddbbd0dd809d4bdfbd7d41be5587a2
madhu <madhu@Madhu>
2024-07-22 08:52:54 +0530

    first commit

--- src/com/Project/JavaProg.java
+++ src/com/Project/JavaProg.java
@@ -3,7 +3,7 @@ package com.Project;
 public class JavaProg {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("madhu");
 		// TODO Auto-generated method stub
 
 	}
---
+++ src/com/Project/Test106.java
@@ -0,0 +1,31 @@
+package com.Project;
+
+import java.util.Arrays;
+
+public class Test106 {
+	boolean[]meth1(){
+		/*
+		boolean flag[]=new boolean[3];
+		flag[0]=true;
+		flag[1]=false;
+		flag[2]=true;
+		return flag;
+		*/
+		return new boolean[] {true,false,true};
+	}
+	void meth2(int arr[]) {
+		System.out.println("meth2 called");
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void main(String[] args) {
+		Test106 obj=new Test106();
+		boolean result[]=obj.meth1();
+		System.out.println(Arrays.toString(result));
+		
+		obj.meth2(new int[] {11,22,33,44});
+		// TODO Auto-generated method stub
+
+	}
+
+}
